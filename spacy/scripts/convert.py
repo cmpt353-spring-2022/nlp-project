69e168513370caa3ad8bc544d08c33c901cdddc3
Anthony Ting <anthonydting@gmail.com>
2022-04-13 02:00:44 -0700

    use cleaned data for spacy

--- spacy/scripts/convert.py
+++ spacy/scripts/convert.py
@@ -6,46 +6,53 @@ import spacy
 from spacy.tokens import DocBin
 
 
-def main(training_ratio: float):
-    clickbait = pd.read_csv("../clickbait.csv.gz")
-    clickbait["created"] = pd.to_datetime(clickbait["created"], unit="s")
-    clickbait["text"] = clickbait["title"].str.split("|").str.get(0)
-    clickbait = clickbait[clickbait["score"] > 5]
-    clickbait["is_clickbait"] = True
-
-    clickbait = clickbait[clickbait["text"].notna()]
-
-    apnews = pd.read_csv("../apnews.csv.gz")
-    apnews["text"] = apnews["title"]
-    apnews = apnews[apnews["text"].notna()]
-    apnews["created"] = pd.to_datetime(apnews["created"], unit="s")
-    apnews["is_clickbait"] = False
-    apnews = apnews.sample(frac=1).reset_index(drop=True)
-
-    assert len(clickbait) < len(apnews)
-    assert type(training_ratio) == float
-
+def main(training_ratio: float, use_filtered: bool = True):
+    assert type(training_ratio) == float    
     print(f"Using training ratio of {training_ratio}")
 
-    training_count = int(training_ratio * len(clickbait))
+    if (not use_filtered):
+        clickbait = pd.read_csv("../clickbait.csv.gz")
+        clickbait["created"] = pd.to_datetime(clickbait["created"], unit="s")
+        clickbait["title"] = clickbait["title"].str.split("|").str.get(0)
+        clickbait = clickbait[clickbait["score"] > 5]
+        clickbait["clickbait"] = 1
 
-    train_data = pd.concat(
-        [apnews[:training_count], clickbait[:training_count]], ignore_index=True
-    )
-    train_data = train_data.sample(frac=1).reset_index(drop=True)
+        clickbait = clickbait[clickbait["title"].notna()]
 
-    valid_data = pd.concat(
-        [apnews[training_count : len(clickbait)], clickbait[training_count:]],
-        ignore_index=True,
-    )
-    valid_data = valid_data.sample(frac=1).reset_index(drop=True)
+        apnews = pd.read_csv("../apnews.csv.gz")
+        apnews = apnews[apnews["title"].notna()]
+        apnews["created"] = pd.to_datetime(apnews["created"], unit="s")
+        apnews["clickbait"] = 0
+        apnews = apnews.sample(frac=1).reset_index(drop=True)
+
+        assert len(clickbait) < len(apnews)
+
+        training_count = int(training_ratio * len(clickbait))
+
+        train_data = pd.concat(
+            [apnews[:training_count], clickbait[:training_count]], ignore_index=True
+        )
+        train_data = train_data.sample(frac=1).reset_index(drop=True)
+
+        valid_data = pd.concat(
+            [apnews[training_count : len(clickbait)], clickbait[training_count:]],
+            ignore_index=True,
+        )
+        valid_data = valid_data.sample(frac=1).reset_index(drop=True)
+    else:
+        data = pd.read_csv('../filtered.csv.gz')
+
+        training_count = int(training_ratio * len(data))
+        
+        train_data = data[:training_count]
+        valid_data = data[training_count:]
 
     def to_corpus(data: pd.DataFrame, filename: str):
         nlp = spacy.blank("en")
         db = DocBin()
         for i, row in data.iterrows():
-            doc = nlp.make_doc(row["text"])
-            if row["is_clickbait"]:
+            doc = nlp.make_doc(row["title"])
+            if row["clickbait"]:
                 doc.cats = {"clickbait": 1.0, "other": 0.0}
             else:
                 doc.cats = {"clickbait": 0.0, "other": 1.0}
