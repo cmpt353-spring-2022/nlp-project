664092d213e6145f1aeb490e33a2efdd88f1cf9d
Anthony Ting <anthonydting@gmail.com>
2022-04-14 10:15:15 -0700

    refactor analysis to new file

---
+++ analyze.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pymannkendall as mk
+
+def get_p_value(subreddit: pd.DataFrame):
+    cb_pred = subreddit[subreddit.prediction == 1]
+    cb_grouped = cb_pred.groupby("Y_M").count()
+    count = subreddit.groupby("Y_M").count()
+    count['cb_ratio'] = cb_grouped["prediction"] / count["prediction"]
+    # count = count[count.author > 100]
+    count = count.reset_index()
+    mk_res = mk.original_test(count['cb_ratio'])
+    return mk_res
+
+#grouped by year for visualization
+def get_cb_ratio(subreddit: pd.DataFrame):
+    cb_pred = subreddit[subreddit.prediction == 1]
+    cb_grouped = cb_pred.groupby("year").count()
+
+    count = subreddit.groupby("year").count()
+    count['cb_ratio'] = cb_grouped["prediction"] / count["prediction"]
+    count = count.reset_index()
+    # count = count[count.author > 500]
+
+    p_value = get_p_value(subreddit)
+    return count, p_value
+
+
+
--- scikit.py
+++ scikit.py
@@ -4,27 +4,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB, ComplementNB, MultinomialNB
 from sklearn.linear_model import Perceptron
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import Binarizer
 import seaborn as sns
 import matplotlib.ticker as mticker
 import pymannkendall as mk
+import analyze
 
-
-
-apnews = pd.read_csv("apnews.csv.gz")
-apnews["title_before_bar"] = apnews.title.str.split(r'|').str.get(0)
-apnews["title_after_bar"] = apnews.title.str.split(r'|').str.get(1)
-apnews["category"] = "not_a_clickbait"
-
-
-clickbait = pd.read_csv("clickbait.csv.gz")
-clickbait["title_before_bar"] = clickbait.title.str.split(r'|').str.get(0)
-clickbait["title_after_bar"] = clickbait.title.str.split(r'|').str.get(1)
-clickbait["category"] = "clickbait"
-
-combined = pd.concat([apnews, clickbait])
-X_train, X_test, y_train, y_test = train_test_split(combined.title_before_bar, combined.category)
+combined = pd.read_csv("data/filtered.csv.gz")
+X_train, X_test, y_train, y_test = train_test_split(combined.title, combined.clickbait)
 
 print()
 
@@ -138,54 +126,33 @@ model_percp.fit(X_train, y_train)
 
 #groups it by month of the year to get enough data points for statistical testing
 
-sub_data_files = ["UpliftingNews_lg.csv.gz", "nottheonion_lg.csv.gz"]
-
-def get_p_value(subreddit):
-    cb_pred = subreddit[subreddit.prediction == "clickbait"]
-    cb_grouped = cb_pred.groupby("Y_M").count()
-    count = subreddit.groupby("Y_M").count()
-    count['cb_ratio'] = cb_grouped["prediction"] / count["prediction"]
-    count = count[count.author > 100]
-    count = count.reset_index()
-    mk_res = mk.original_test(count['cb_ratio'])
-    return mk_res
-
-#grouped by year for visualization
-def get_cb_ratio(subreddit):
-    subreddit['created'] = pd.to_datetime(subreddit["created"], unit="s")
-    subreddit['year'] = pd.DatetimeIndex(subreddit['created']).year
-    subreddit['Y_M'] = pd.DatetimeIndex(subreddit['created']).to_period("M")
-    subreddit['prediction'] = model_percp.predict(subreddit.title)
-    
-    
-    cb_pred = subreddit[subreddit.prediction == "clickbait"]
-    cb_grouped = cb_pred.groupby("year").count()
-
-    count = subreddit.groupby("year").count()
-    count['cb_ratio'] = cb_grouped["prediction"] / count["prediction"]
-    count = count.reset_index()
-    count = count[count.author > 500]
-
-    p_value = get_p_value(subreddit)
-    return count, p_value
+sub_data_files = ["data/UpliftingNews_lg.csv.gz", "data/nottheonion_lg.csv.gz", "data/worldnews_lg.csv.gz", "data/politics_lg.csv.gz"]
 
 
-
+def predict(subreddit: str, model: Pipeline):
+    df = pd.read_csv(subreddit)
+    df = df.dropna(subset=['title'])
+    if ('created' in df.columns):
+        df['created'] = pd.to_datetime(df["created"], unit="s")
+    else:
+        df['created'] = df['created_utc']
+    df['year'] = pd.DatetimeIndex(df['created']).year
+    df['Y_M'] = pd.DatetimeIndex(df['created']).to_period("M")
+    df['prediction'] = model.predict(df.title)
+    # maybe save these predictions, so we don't have to re-run this
+    return df
 
 
 for file in sub_data_files:
-    subreddit = pd.read_csv(file)
-    subreddit = subreddit.dropna(subset=['title'])
-    output, test_res = get_cb_ratio(subreddit)
+    df = predict(file, model_percp)
+    output, test_res = analyze.get_cb_ratio(df)
     print(test_res)
-    plt.plot(output.year, output.cb_ratio)
-
-
-
+    plt.plot(output.year, output.cb_ratio, label=file)
 
 
 sns.set_theme()
 
+plt.legend(loc="upper left")
 plt.xlabel("Year")
 plt.ylabel("% of clickbait titles")
 plt.title("Percentage of clickbait titles in selected news subreddits over the years")
