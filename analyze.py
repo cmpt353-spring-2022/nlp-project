7879543d49e5505bd068e216540066b5e6d60666
Puneet Sandhu <pssarhali@gmail.com>
2022-04-15 21:20:44 -0700

    minor changes

--- analyze.py
+++ analyze.py
@@ -8,7 +8,7 @@ def get_p_value(subreddit: pd.DataFrame):
     cb_grouped = cb_pred.groupby("Y_M").count()
     count = subreddit.groupby("Y_M").count()
     count['cb_ratio'] = cb_grouped["prediction"] / count["prediction"]
-    count = count[count.created_utc > 100]
+    count = count[count.created_utc > 50]
     count = count.reset_index()
     mk_res = mk.original_test(count['cb_ratio'])
     return mk_res
@@ -21,7 +21,7 @@ def get_cb_ratio(subreddit: pd.DataFrame):
     count = subreddit.groupby("year").count()
     count['cb_ratio'] = cb_grouped["prediction"] / count["prediction"]
     count = count.reset_index()
-    count = count[count.created_utc > 500]
+    count = count[count.created_utc > 50]
 
     p_value = get_p_value(subreddit)
     return count, p_value
@@ -35,7 +35,7 @@ def ratio_by_score(subreddit: pd.DataFrame, partitions):
     df = df.sort_values(by="score").reset_index(drop=True)
     df["part_num"] = np.vectorize(partition_num)(df.index, df.shape[0], partitions)
     df = df[df.prediction == 1]
-    df = df.groupby(by="part_num").sum().reset_index(drop=True)
+    df = df.groupby(by="part_num").agg(score=('score', 'mean'), prediction=('prediction', 'count')).reset_index(drop=True)
     df["prediction"] = df["prediction"] / (length/partitions)
     return df
 
